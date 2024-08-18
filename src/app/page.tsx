9268569a08451fec4c099813a7450d22b8f0aeb4
Bimal Gurung <bimalgrg519@gmail.com>
2024-08-18 11:52:59 +0545

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,264 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+function shuffleTeams(pairs: any) {
+  const teamA: any = [];
+  const teamB: any = [];
+
+  pairs.forEach((pair: any) => {
+    // Randomly assign the first member of the pair to either teamA or teamB
+    if (Math.random() > 0.5) {
+      teamA.push(pair[0]);
+      teamB.push(pair[1]);
+    } else {
+      teamA.push(pair[1]);
+      teamB.push(pair[0]);
+    }
+  });
+
+  return { teamA, teamB };
+}
 
 export default function Home() {
+  const [playerName, setPlayerName] = useState("");
+  const [players, setPlayers] = useState<any[]>([
+    {
+      id: 1,
+      name: "Bimal",
+    },
+    {
+      id: 2,
+      name: "Amir",
+    },
+    {
+      id: 3,
+      name: "Shankar",
+    },
+    {
+      id: 4,
+      name: "Sirdar",
+    },
+    {
+      id: 5,
+      name: "Bikal",
+    },
+    {
+      id: 6,
+      name: "Prakash",
+    },
+    {
+      id: 7,
+      name: "Sanish",
+    },
+    {
+      id: 8,
+      name: "Kushal",
+    },
+    {
+      id: 9,
+      name: "Nirajan",
+    },
+    {
+      id: 10,
+      name: "Gange",
+    },
+    {
+      id: 11,
+      name: "Aagaman",
+    },
+    {
+      id: 12,
+      name: "Jiwon",
+    },
+    {
+      id: 13,
+      name: "Rajin",
+    },
+    {
+      id: 14,
+      name: "Sanjaya",
+    },
+    {
+      id: 15,
+      name: "Sunil",
+    },
+  ]);
+
+  const [teamPair, setTeamPair] = useState<any[]>([]);
+
+  const [selectedPlayer1, setSelectedPlayer1] = useState<any>("");
+  const [selectedPlayer2, setSelectedPlayer2] = useState<any>("");
+
+  const [teamA, setTeamA] = useState<any[]>([]);
+  const [teamB, setTeamB] = useState<any[]>([]);
+
+  const handleSubmit = () => {
+    if (selectedPlayer1 && selectedPlayer2) {
+      const filteredPlayers = players.filter(
+        (p) => p.id !== selectedPlayer1.id && p.id !== selectedPlayer2.id
+      );
+      setPlayers(filteredPlayers);
+      setTeamPair([...teamPair, [selectedPlayer1, selectedPlayer2]]);
+      setSelectedPlayer1("");
+      setSelectedPlayer2("");
+    }
+  };
+
+  const handleSuffle = () => {
+    const { teamA, teamB } = shuffleTeams(teamPair);
+    setTeamA(teamA);
+    setTeamB(teamB);
+  };
+
+  const handleAdd = () => {
+    if (playerName) {
+      const newPlayer = {
+        id: players.length + 1,
+        name: playerName,
+      };
+      setPlayers([...players, newPlayer]);
+      setPlayerName("");
+    }
+  };
+
+  const handleRemove = (removedPlayers) => {
+    const [player1, player2] = removedPlayers;
+
+    setTeamPair(
+      teamPair.filter((p) => p[0].id !== player1.id && p[1].id !== player2.id)
+    );
+    setPlayers([...players, ...removedPlayers]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <div className="z-10 w-full max-w-5xl font-mono">
+        <h1 className="text-xl">LC Brotherhood</h1>
+        <div className="flex space-x-2">
+          <input
+            className="text-black px-2"
+            value={playerName}
+            onChange={(e) => {
+              setPlayerName(e.target.value);
+            }}
+          />
+          <button className="bg-blue-300 px-4 text-black" onClick={handleAdd}>
+            Add
+          </button>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex gap-x-3 gap-y-3 mt-3 flex-wrap">
+          {players.map((player) => (
+            <div key={player.id} className="border px-2">
+              <h1>{player.name}</h1>
+            </div>
+          ))}
+        </div>
+        <div className="mt-8">
+          <div className="w-full flex p-2 space-x-2">
+            <select
+              className="text-black w-full"
+              value={selectedPlayer1 ? selectedPlayer1.id : ""}
+              onChange={(e) => {
+                setSelectedPlayer1(
+                  players.find((player) => player.id === Number(e.target.value))
+                );
+              }}
+            >
+              <option value="" disabled>
+                Player 1
+              </option>
+              {players.map((player) => (
+                <option key={player.id} value={player.id} className="text-xl">
+                  {player.name}
+                </option>
+              ))}
+            </select>
+            <select
+              className="text-black w-full"
+              value={selectedPlayer2 ? selectedPlayer2.id : ""}
+              onChange={(e) => {
+                setSelectedPlayer2(
+                  players.find((player) => player.id === Number(e.target.value))
+                );
+              }}
+            >
+              <option value="" disabled>
+                Player 2
+              </option>
+              {players.map((player) => (
+                <option key={player.id} value={player.id}>
+                  {player.name}
+                </option>
+              ))}
+            </select>
+            <button
+              className="bg-blue-300 px-4 text-black"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+          <div className="flex space-x-2">
+            <div className="flex-1">
+              <div className="flex">
+                <h2 className="flex-1">Team 1</h2>
+                <h2 className="flex-1">Team 2</h2>
+              </div>
+              {
+                <div className="space-y-2">
+                  {teamPair.map((players: any, index) => (
+                    <div key={index} className="flex space-x-2">
+                      <div className="flex-1 border px-2">
+                        <h1>{players[0].name}</h1>
+                      </div>
+                      <div className="flex-1 border px-2">
+                        <h1>{players[1].name}</h1>
+                      </div>
+                      <button
+                        className="bg-red-600 px-4 rounded"
+                        onClick={() => handleRemove(players)}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              }
+            </div>
+          </div>
+          <button
+            onClick={handleSuffle}
+            className="bg-blue-200 text-black p-2 rounded my-4"
+          >
+            Suffle
+          </button>
+          <div className="flex">
+            <div className="flex-1">
+              <h2>Team 1</h2>
+              {
+                <div className="flex flex-col space-y-2">
+                  {teamA.map((player: any) => (
+                    <div key={player.id} className="border px-2">
+                      <h1>{player.name}</h1>
+                    </div>
+                  ))}
+                </div>
+              }
+            </div>
+            <div className="flex-1">
+              <h2>Team 2</h2>
+              {
+                <div className="flex flex-col space-y-2">
+                  {teamB.map((player: any) => (
+                    <div key={player.id} className="border px-2">
+                      <h1>{player.name}</h1>
+                    </div>
+                  ))}
+                </div>
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
